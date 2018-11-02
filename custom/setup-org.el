8e7951739f599a78a9ccd7168c7539c6297d7616
Jonathan CARRIERE <jcarriere@ausy-group.com>
2018-11-02 15:34:03 +0100

    Dev: org-mode customization is now possible.
    Signed-off-by: Jonathan CARRIERE <jcarriere@ausy-group.com>

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -1,4 +1,17 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; org mode customisation is done            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (setq org-todo-keywords
-      '((sequence "TODO" "DELEGATED" "STOPPED" "DONE" )))
+      '((sequence "TODO" "DELEGATED" "STOPPED" "WAITING" "DONE" )))
+
+(add-hook 'org-mode-hook (lambda ()
+                           ;; (ispell-change-dictionary "en")
+                           (flyspell-mode)
+                           (set-fill-column 80)
+                           (auto-fill-mode)
+                           (bline-minor-mode)
+                           ))
+
 
 (provide 'setup-org)
