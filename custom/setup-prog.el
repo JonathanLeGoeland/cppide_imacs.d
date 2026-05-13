2b63e765cdecaa8d441e50b19a4c3926b6bcbe4e
Jonathan <carriere.jonathan@gmail.com>
2026-05-13 11:57:59 +0200

    Fancy-fill-paragraph.

--- custom/setup-prog.el
+++ custom/setup-prog.el
@@ -1,1 +1,6 @@
+;; Essentiellement pour respecter mes listes à points dans les
+;; commentaires de mon code quand on fait M-q (fill-paragraph).
+(use-package fancy-fill-paragraph
+  :bind ("M-q" . fancy-fill-paragraph))
+
 (provide 'setup-prog)
