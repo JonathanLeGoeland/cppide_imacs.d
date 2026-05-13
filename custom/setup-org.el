0119a3c69f2b7e686ace134e54b361301d0b4ec8
Jonathan <carriere.jonathan@gmail.com>
2026-05-13 12:06:11 +0200

    Définition de couleurs de titres plus lisibles dans org-mode.

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -21,81 +21,21 @@
 (add-hook 'org-mode-hook
           (lambda () (local-set-key (kbd "C-c C-x t") #'ispell-change-dictionary)))
 
-
-;; (if window-system
-;;     (progn
-;;       ;; We try to beautify org mode only when windowing is availabel.
-;;       ;; Hide the style marks
-;;       ;;(setq org-hide-emphasis-markers t)
-;;       ;; Better bulltets
-;;       (font-lock-add-keywords 'org-mode
-;;                               '(("^ +\\([-*]\\) "
-;;                                  (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-;;       ;; Better header bullets
-;;       (require 'org-bullets)
-;;       (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-
-;;       ;; Better headers
-;;       (let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-;;                                    ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-;;                                    ((x-list-fonts "Verdana")         '(:font "Verdana"))
-;;                                    ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-;;                                    (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-;;              (base-font-color     (face-foreground 'default nil 'default))
-;;              (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-;;         (custom-theme-set-faces 'user
-;;                                 `(org-level-8 ((t (,@headline ,@variable-tuple))))
-;;                                 `(org-level-7 ((t (,@headline ,@variable-tuple))))
-;;                                 `(org-level-6 ((t (,@headline ,@variable-tuple))))
-;;                                 `(org-level-5 ((t (,@headline ,@variable-tuple))))
-;;                                 `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-;;                                 `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-;;                                 `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-;;                                 `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-;;                                 `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
-
-;;       (set-face-attribute 'org-meta-line nil :height 1.0 :slant 'normal :background "gray")
-;;       (setq org-src-fontify-natively t)
-;;       ))
-
-
-;; Keep ?
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(org-document-title ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.5 :underline nil))))
-;;  '(org-level-1 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.75))))
-;;  '(org-level-2 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.5))))
-;;  '(org-level-3 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.25))))
-;;  '(org-level-4 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.1))))
-;;  '(org-level-5 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
-;;  '(org-level-6 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
-;;  '(org-level-7 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
-;;  '(org-level-8 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
-;;  '(rainbow-delimiters-depth-1-face ((t (:foreground "red" :weight bold))))
-;;  '(rainbow-delimiters-depth-2-face ((t (:foreground "orange" :weight bold))))
-;;  '(rainbow-delimiters-depth-3-face ((t (:foreground "cyan" :weight bold))))
-;;  '(rainbow-delimiters-depth-4-face ((t (:foreground "green" :weight bold))))
-;;  '(rainbow-delimiters-depth-5-face ((t (:foreground "blue" :weight bold))))
-;;  '(rainbow-delimiters-depth-6-face ((t (:foreground "violet" :weight bold))))
-;;  '(rainbow-delimiters-depth-7-face ((t (:foreground "purple" :weight bold))))
-;;  '(rainbow-delimiters-depth-8-face ((t (:foreground "black" :weight bold))))
-;;  '(rainbow-delimiters-unmatched-face ((t (:background "red" :weight bold)))))
-
-        (custom-theme-set-faces 'user
-                                `(org-level-1 ((t (:background nil :extend t :weight bold :foreground "red" :underline t))))
-                                `(org-level-2 ((t (:background nil :extend t :weight bold :foreground "orange" :underline t))))
-                                `(org-level-3 ((t (:background nil :extend t :weight bold :foreground "blue" :underline t))))
-                                `(org-level-4 ((t (:background nil :extend t :weight bold :foreground "SpringGreen" :underline t))))
-                                `(org-level-5 ((t (:background nil :extend t :weight bold :foreground "cyan" :underline t))))
-                                `(org-level-6 ((t (:background nil :extend t :weight bold :foreground "DarkViolet" :underline t))))
-                                `(org-level-7 ((t (:background nil :extend t :weight bold :foreground "DarkTurquoise" :underline t))))
-                                `(org-level-8 ((t (:background nil :extend t :weight bold :foreground "CadetBlue" :underline t)))))
-                                ;;`(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Customisation des fontes
+(custom-theme-set-faces
+ 'user
+ '(org-block-begin-line ((t (:underline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t))))
+ '(org-block-end-line ((t (:overline "#A7A6AA" :foreground "#008ED1" :background "#EAEAFF" :extend t))))
+ `(org-level-1 ((t (:background nil :extend t :weight bold :foreground "red" :underline t))))
+ `(org-level-2 ((t (:background nil :extend t :weight bold :foreground "orange" :underline t))))
+ `(org-level-3 ((t (:background nil :extend t :weight bold :foreground "blue" :underline t))))
+ `(org-level-4 ((t (:background nil :extend t :weight bold :foreground "green" :underline t))))
+ `(org-level-5 ((t (:background nil :extend t :weight bold :foreground "magenta" :underline t))))
+ `(org-level-6 ((t (:background nil :extend t :weight bold :foreground "DarkViolet" :underline t))))
+ `(org-level-7 ((t (:background nil :extend t :weight bold :foreground "DarkTurquoise" :underline t))))
+ `(org-level-8 ((t (:background nil :extend t :weight bold :foreground "CadetBlue" :underline t)))))
+;;`(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil)))))
 
 
 
@@ -420,5 +360,14 @@ org-roam folder is given by variable org-roam-directory
          "* %?"
          :target (file+head "%<%Y-%m-%d>.org"
                             "#+title: %<%Y-%m-%d>\n"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Affichage de la tâche en cour dans la ligne de frame:
+(setq org-clock-clocked-in-display 'frame-title)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Utiliseation de htmlize pour la coloration syntaxique des
+;; blocs de code exportés vers HTML.
+(use-package htmlize)
 
 (provide 'setup-org)
