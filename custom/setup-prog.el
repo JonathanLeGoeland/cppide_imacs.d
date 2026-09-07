91ab236dd4bb0d734f279d20fd572e10ade07ab4
Jonathan <carriere.jonathan@gmail.com>
2026-09-07 17:24:01 +0200

    Suppression de l'utilisation de fancy-fill-paragrph et renumération.
    Renumérotation des listes numérotées dans le code.

--- custom/setup-prog.el
+++ custom/setup-prog.el
@@ -1,6 +1,21 @@
 ;; Essentiellement pour respecter mes listes à points dans les
 ;; commentaires de mon code quand on fait M-q (fill-paragraph).
-(use-package fancy-fill-paragraph
-  :bind ("M-q" . fancy-fill-paragraph))
+;; (use-package fancy-fill-paragraph
+;;   :bind ("M-q" . fancy-fill-paragraph))
+
+;; ============================================================================
+;; Renumbering des listes dans le code
+(defcustom number-regex "# [0-9]+ -" "Expression régulière de numérotation")
+(defun renumber-region (start end)
+  (interactive "r")
+  (let ((count 1)
+        (replacement (string-replace "[0-9]+" "%d" number-regex)))
+    (save-excursion
+      (goto-char start)
+      ;; (message "-%s-" replacement)
+      (while (re-search-forward number-regex end t)
+        (replace-match (format replacement count))
+        ;; (message "Replacement %d" count)
+        (setq count (+ count 1))))))
 
 (provide 'setup-prog)
